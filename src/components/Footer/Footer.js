c1bcc00074be9dce8c8926ba5fd2ed4f396c95f1
Urmat Bekboev <bek.urmat@gmail.com>
2019-01-15 04:04:27 +0600

    Added footer to project

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <div className="resources">
+                    <h4>Resources</h4>
+                    <p>Easily manage your inspiration and work-in-progress by dragging images into projects and sharable
+                        client groups. work in-progress by dragging images into projects and sharable client group</p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
