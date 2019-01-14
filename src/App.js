a18e5a2d2b3a21a137c10f591f988f885b808474
Urmat Bekboev <bek.urmat@gmail.com>
2019-01-15 03:58:27 +0600

    Fixed warnings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Header from "./components/Header/Header";
+import Articles from "./components/Articles/Articles";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header />
+          <Articles/>
       </div>
     );
   }
