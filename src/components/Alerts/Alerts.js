2db724192dd88588cdcba01088afb3626a4e1134
Urmat Bekboev <bek.urmat@gmail.com>
2019-01-15 16:27:34 +0600

    Code refactor made

--- src/components/Alerts/Alerts.js
+++ src/components/Alerts/Alerts.js
@@ -3,35 +3,26 @@ import Alert from "../../UI/Alert/Alert";
 
 const Alerts = () => {
     const someHandler = () => null;
+    const alerts = [
+        {type: 'primary', show: true, dismiss: someHandler},
+        {type: 'success', show: true, dismiss: someHandler},
+        {type: 'warning', show: true},
+        {type: 'danger', show: true, dismiss: someHandler},
+        {type: 'primary', show: false, dismiss: someHandler},
+        {type: 'success', show: true}
+    ];
+
     return (
         <div className="alerts container">
             <h2 style={{color: "#a2ca28"}}>Alerts examples:</h2>
-            <Alert
-                type="primary"
-                show={true}
-                dismiss={someHandler}
-            >This is a primary type alert</Alert>
-            <Alert type="success">This is a primary type alert</Alert>
-
-            <Alert
-                type="warning"
-                show={true}
-            >This is a warning type alert</Alert>
-            <Alert type="success">This is a warning type alert without dismiss !</Alert>
-
-            <Alert
-                type="success"
-                show={true}
-                dismiss={someHandler}
-            >This is a success type alert</Alert>
-            <Alert type="danger">This is a success type alert</Alert>
-
-            <Alert
-                type="danger"
-                show={true}
-                dismiss={someHandler}
-            >This is a danger type alert</Alert>
-            <Alert type="danger">This is a danger type alert</Alert>
+            {alerts.map((alert,index)=> (
+                <Alert
+                    key={index}
+                    type={alert.type}
+                    show={alert.show}
+                    dismiss={alert.dismiss}
+                >This is a {alert.type} type alert</Alert>
+            ))}
         </div>
     );
 };
